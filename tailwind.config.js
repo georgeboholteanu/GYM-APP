a3aed71641ce0d6017f6807dec8c45fcf67117df
georgeb <george.boholteanu@gmail.com>
2023-01-01 19:42:44 +0000

    update links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
   
   plugins: [
-    require('./node_modules/flowbite/plugin')
+    require('flowbite/plugin')
   ]
 }
